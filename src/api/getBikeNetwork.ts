b99737bb2f9112bac60aa55879ca268629dc8abc
KikoUguina <kipuguina@gmail.com>
2025-04-24 18:50:28 +0200

    Added: fetch and display of Spanish bike networks

--- src/api/getBikeNetwork.ts
+++ src/api/getBikeNetwork.ts
@@ -1,3 +1,15 @@
-export const getBikeNetworks = async () => {
-    return;
+import axios from 'axios'
+
+type Network = {
+    id: string;
+    name: string;
+    company?: string[];
+    location: {
+        city: string;
+        country: string;
+    };
+};
+export const getBikeNetworks = async (): Promise<Network[]> => {
+    const response = await axios.get('https://api.citybik.es/v2/networks/')
+    return response.data.networks.filter((network: any) => network.location.country === 'ES');  
 };
